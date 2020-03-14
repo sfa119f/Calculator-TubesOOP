d2229d654627367862d7ec45277a43db544ac2de
sfa119f <syarifuddinfa@gmail.com>
2020-03-14 22:39:54 +0700

    buat startform(minor error, mainform tidak muncul di jendela teratas namun di jendela kedua) dan perubahan nama method

---
+++ MainForm.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainForm : Form
+    {
+        static private bool isDecimal = false;
+        static private bool isAnswer = false;
+        static private bool isOpInLast = false;
+        static private bool isNegative = false;
+        static private List<bool> numberDecimal = new List<bool>();
+
+        public MainForm()
+        {
+            Thread t = new Thread(new ThreadStart(startingForm));
+            t.Start();
+            Thread.Sleep(1500);
+            InitializeComponent();
+            t.Abort();
+        }
+
+        public void startingForm()
+        {
+            Application.Run(new StartForm());
+        }
+
+        public async void waitingTime()
+        {
+            await Task.Delay(5000);
+        }
+
+        private void buttonEmptyMemoryClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void makeEmptyScreen(string temp, bool check)
+        {
+            isOpInLast = false; //digunakan untuk multi operasi
+            isAnswer = false;
+            if (temp == "0" || check)
+            {
+                labelScreen.Text = "";
+                isAnswer = false;
+            }
+        }
+
+        private void buttonNum0Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "0";
+        }
+
+        private void buttonNum1Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "1";
+        }
+
+        private void buttonNum2Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "2";
+        }
+
+        private void buttonNum3Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "3";
+        }
+
+        private void buttonNum4Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "4";
+        }
+
+        private void buttonNum5Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "5";
+        }
+
+        private void buttonNum6Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "6";
+        }
+
+        private void buttonNum7Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "7";
+        }
+
+        private void buttonNum8Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "8";
+        }
+
+        private void buttonNum9Click(object sender, EventArgs e)
+        {
+            makeEmptyScreen(labelScreen.Text, isAnswer);
+            labelScreen.Text += "9";
+        }
+
+        private void buttonDesimalClick(object sender, EventArgs e)
+        {
+            if (!isDecimal)
+            {
+                isDecimal = true;
+                labelScreen.Text += ",";
+            }
+        }
+
+        private void buttonDeleteClick(object sender, EventArgs e)
+        {
+            string temp = labelScreen.Text.Substring(labelScreen.Text.Length - 1);
+            if (isAnswer)
+            {
+                isAnswer = false;
+            }
+            else if (temp == ",")
+            {
+                isDecimal = false;
+            }
+            else if ((temp == "+" || temp == "*" || temp == "÷" || temp == "-") && !isNegative)
+            {
+                isOpInLast = false;
+                isDecimal = numberDecimal[numberDecimal.Count-1];
+                numberDecimal.RemoveAt(numberDecimal.Count - 1);
+            }
+            labelScreen.Text = labelScreen.Text.Remove(labelScreen.Text.Length - 1);
+            if (labelScreen.Text.Length == 0)
+            {
+                labelScreen.Text = "0";
+            }
+        }
+
+        private void buttonAllClearClick(object sender, EventArgs e)
+        {
+            labelScreen.Text = "0";
+            isDecimal = false;
+            numberDecimal.Clear();
+            isAnswer = false;
+            isOpInLast = false;
+        }
+
+        private void buttonSumClick(object sender, EventArgs e)
+        {
+            if (!isNegative)
+            {
+                if (isOpInLast)
+                {
+                    labelScreen.Text = labelScreen.Text.Remove(labelScreen.Text.Length - 1);
+                }
+                labelScreen.Text += "+";
+                isOpInLast = true;
+                numberDecimal.Add(isDecimal);
+            }
+            isDecimal = false;
+            isAnswer = false;
+        }
+
+        private void buttonSubtractionClick(object sender, EventArgs e)
+        {
+            string temp = labelScreen.Text.Substring(labelScreen.Text.Length - 1);
+            if (temp == "*" || temp == "÷")
+            {
+                isNegative = true;
+            }
+            else if (temp == "+" || temp == "-")
+            {
+                labelScreen.Text = labelScreen.Text.Remove(labelScreen.Text.Length - 1);
+                numberDecimal.Add(isDecimal);
+            }
+            labelScreen.Text += "-";
+            isOpInLast = true;
+            isDecimal = false;
+            isAnswer = false;
+        }
+
+        private void buttonMultiplicationClick(object sender, EventArgs e)
+        {
+            if (!isNegative)
+            {
+                if (isOpInLast)
+                {
+                    labelScreen.Text = labelScreen.Text.Remove(labelScreen.Text.Length - 1);
+                }
+                labelScreen.Text += "*";
+                isOpInLast = true;
+                numberDecimal.Add(isDecimal);
+            }
+            isDecimal = false;
+            isAnswer = false;
+        }
+
+        private void buttonDivisionClick(object sender, EventArgs e)
+        {
+            if (!isNegative)
+            {
+                if (isOpInLast)
+                {
+                    labelScreen.Text = labelScreen.Text.Remove(labelScreen.Text.Length - 1);
+                }
+                labelScreen.Text += "÷";
+                isOpInLast = true;
+                numberDecimal.Add(isDecimal);
+            }
+            isDecimal = false;
+            isAnswer = false;
+        }
+
+        private void buttonRootClick(object sender, EventArgs e)
+        {
+            labelScreen.Text += "√";
+        }
+
+        private void buttonResultClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ StartForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Calculator
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
+            this.BackGround = new System.Windows.Forms.PictureBox();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.BackGround)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BackGround
+            // 
+            this.BackGround.Image = global::Calculator.Properties.Resources.iconfinder_09_Calculator_1871426;
+            this.BackGround.Location = new System.Drawing.Point(-1, 0);
+            this.BackGround.Name = "BackGround";
+            this.BackGround.Size = new System.Drawing.Size(250, 250);
+            this.BackGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackGround.TabIndex = 0;
+            this.BackGround.TabStop = false;
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Location = new System.Drawing.Point(30, 217);
+            this.ProgressBar.MarqueeAnimationSpeed = 50;
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(185, 20);
+            this.ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.ProgressBar.TabIndex = 1;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 250);
+            this.ControlBox = false;
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.BackGround);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.BackGround)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BackGround;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+    }
+}
